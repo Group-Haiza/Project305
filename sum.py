7143bc95e7aeb7ee2e40f859a9c96ed373daf01f
afifahliyana <67133411+afifahliyana@users.noreply.github.com>
2020-06-19 19:51:50 +0800

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+#function to sum() the value from user input
+
+def sum (x,y):
+  return x+y
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c= sum (a,b)
+print("Sum of a", a, "and b", b, "is", c)
