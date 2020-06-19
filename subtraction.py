9a6c505212c0f4da2e7f0b15f1298f89763402a7
nurfatinalwannie <67133604+nurfatinalwannie@users.noreply.github.com>
2020-06-19 17:22:02 +0800

    Add files via upload

---
+++ subtraction.py
@@ -0,0 +1,10 @@
+def subtraction(num1, num2):
+    totminus = num1 - num2
+    return totminus
+
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+result = subtraction(num1, num2)
+print("\n\nSubtraction of two numbers:", result)
