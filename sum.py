fbf69c18173a6f39ce959925e46590b8a0ea8a26
afifahliyana <67133411+afifahliyana@users.noreply.github.com>
2020-06-19 20:12:03 +0800

    Delete sum.py

--- sum.py
+++
@@ -1,9 +0,0 @@
-#function to sum() the value from user input
-
-def sum (x,y):
-  return x+y
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c= sum (a,b)
-print("Sum of a", a, "and b", b, "is", c)
